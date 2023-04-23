9c9ab77f94eec83c9734b15f94dfbb1dd00f66cf
Muhammad Imran <imranonthenet@gmail.com>
2023-04-23 20:08:37 +0300

    Changed H2 tag

--- src/features/user/UserView.jsx
+++ src/features/user/UserView.jsx
@@ -12,7 +12,7 @@ export const UserView = () => {
     }, [])
   return (
     <div>
-        <h2>List of users</h2>
+        <h2>List of existing users</h2>
         {user.loading && <div>Loading...</div>}
         {!user.loading && user.error ? <div>Error: {user.error}</div>: null}
         {!user.loading && user.users.length ? (
